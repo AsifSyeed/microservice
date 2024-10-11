cf94d042d6e8cc3b140d6e43496094664b4e00a4
AsifSyeed <asifsyeed666@gmail.com>
2024-10-11 20:31:35 +0600

    Api Gateway implemented

---
+++ order-service/src/main/java/com/example/orderservice/dto/InventoryResponse.java
@@ -0,0 +1,14 @@
+package com.example.orderservice.dto;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String skuCode;
+    private boolean isInStock;
+}
